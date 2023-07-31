ae411722b794c6739db15e8d1a832ee83c3dd51c
Randiipaulus <Randyulises18@Gmail.com>
2023-07-31 12:16:03 -0400

    randii22

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -12,14 +12,17 @@ export default function HomePage({ setPage }){
 
     return (
         <>
-        <div className="homeDiv">
-            <h1>Home Page</h1>
-            <h3>Welcome to the main page</h3>
-            <button name="index" onClick={handleClick}>Click here to see all articles</button>
-            <button name="postForm" onClick={handleClick}>Click here to add an article</button>
-        </div>
+        
+            <div className="homeDiv">
+                <h1 className='title'>Home Page</h1>
+                <h3 className='second'>Welcome to the main page</h3>
+                    <div className='btt'>
+                        <button name="index" onClick={handleClick}>Click here to see all articles</button>
+                        <button name="postForm" onClick={handleClick}>Click here to add an article</button>
+                    </div>
+            </div>
         </>
    
     )
 
-}
+} 
